e6c2fd0f2e07d876eb3fa26389dd7408aa9e9c23
JJ Cao <jjc3561@hotmail.com>
2020-05-18 00:54:17 -0700

    strawman test script

--- tests/nmc_nlp_lite_test.py
+++ tests/nmc_nlp_lite_test.py
@@ -3,14 +3,15 @@ PKG='nmc_nlp_lite'
 #import roslib; roslib.load_manifest(PKG)  # This line is not needed with Catkin.
 
 import sys
-import unittest
+#import unittest
 
 ## A sample python unit test
-class dummyTest(unittest.TestCase):
+#class dummyTest(unittest.TestCase):
 
-    def test_one_equals_one(self):
-        pass
+#    def test_one_equals_one(self):
+#        pass
 
 if __name__ == '__main__':
-    import rosunit
-    rosunit.unitrun(PKG, 'test_bare_bones', dummyTest)
+    #import rosunit
+    #rosunit.unitrun(PKG, 'test_bare_bones', dummyTest)
+	pass
