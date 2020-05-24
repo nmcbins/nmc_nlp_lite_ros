148ca2f3231cb4396d0779d5688aac87a1f4c42e
JJ Dev <jjdev@nmcomputing.com>
2020-05-24 04:17:11 -0700

    Add usage sample code

---
+++ nmc_nlp_lite_client_example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+from nmc_nlp_lite.srv import *
+import time
+nmcNLPService = rospy.ServiceProxy('nmc_nlp_service', nmcNLP)
+
+def call_nmcNLPService(inputString):
+    rospy.wait_for_service('nmc_nlp_service')
+    try:
+        #req = nmc_nlp_lite.srv.nmcNLP(inputString)
+        nmcRequest =nmcNLPRequest(inputString)
+        nmcResponse = nmcNLPService(nmcRequest)
+        return nmcResponse.binsOutput
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+if __name__ == "__main__":
+	while True:
+		a=call_nmcNLPService("a male b")
+		print "response:" + a
+		time.sleep(1)
